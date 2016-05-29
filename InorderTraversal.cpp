f26aab7a265f47fcf586f145103dab8041a931a5
dansutj <dansutj@sudandeMacBook-Pro.local>
2016-05-29 21:17:20 +0800

    lc
    lc

--- InorderTraversal.cpp
+++ InorderTraversal.cpp
@@ -9,6 +9,40 @@ struct TreeNode {
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        if(!root) return result;
+
+        TreeNode* pre = NULL;
+        TreeNode* cur = root;
+
+        while(cur)
+        {
+        	if(!cur->left)
+        	{
+        		result.push_back(cur->val);
+        		cur = cur->right;
+        	}
+        	else
+        	{
+        		pre = cur->left;
+        		while(pre->right && pre->right != cur)
+        			pre = pre->right;
+
+        		if(!pre->right)
+        		{
+        			pre->right = cur;
+        			cur = cur->left;
+        		}
+        		else 
+        		{
+        			result.push_back(cur->val);
+        			pre->right = NULL;
+        			cur = cur->right;
+        		}
+        	}
+        }
+        return result;
     }
+
+private:
+	vector<int> result;
 };
